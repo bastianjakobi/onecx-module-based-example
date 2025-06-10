787497cda99f2084d16b0a2ffcb8ca93b49401c6
Bastian Jakobi <bastian.jakobi2000@gmail.com>
2025-06-10 15:41:57 +0200

    feat: standalone mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { addInitializeModuleGuard } from '@onecx/angular-integration-interface';
+import { provideStandaloneProviders, StandaloneShellModule } from '@onecx/standalone-shell';
 import { standaloneRoutes } from './app.routes';
+import { commonImports, commonProviders } from './remote.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    ...commonImports,
+    StandaloneShellModule,
     RouterModule.forRoot(addInitializeModuleGuard(standaloneRoutes))
   ],
-  providers: [],
+  providers: [
+    ...commonProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
